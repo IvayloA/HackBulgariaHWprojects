756335b269111617a965a1abc889cb8c7c3ad747
IvayloIvanov <ivailo.ivanov3@gmail.com>
2015-12-27 15:38:39 +0200

    week4day1

---
+++ Week4day1_Tuesday/GenericDequeue/GenericDequeueApplication/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GenericDequeueLibrary;
+
+
+namespace GenericDequeue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list1 = new List<int>() { 1, 3, 5, 7, 9, 12, 16 };
+            GenericDequeue<int> dequeue1 = new GenericDequeue<int>(list1);
+
+            Console.WriteLine(dequeue1.PeekFromFront());
+            Console.WriteLine(dequeue1.PeekFromEnd() + "\n -----");
+            dequeue1.RemoveFromFront();
+            dequeue1.RemoveFromEnd();
+            Console.WriteLine(dequeue1.PeekFromFront());
+            Console.WriteLine(dequeue1.PeekFromEnd() + "\n -----");
+            dequeue1.AddToFront(15);
+            dequeue1.AddToEnd(100);
+            Console.WriteLine(dequeue1.PeekFromFront());
+            Console.WriteLine(dequeue1.PeekFromEnd() + "\n -----");
+
+            List<string> list2 = new List<string>() {"this","earth","word","apple","desktop"};
+            GenericDequeue<string> dequeue2 = new GenericDequeue<string>(list2);
+
+
+            Console.WriteLine(dequeue2.PeekFromFront());
+            Console.WriteLine(dequeue2.PeekFromEnd() + "\n -----");
+            dequeue2.RemoveFromFront();
+            dequeue2.RemoveFromEnd();
+            Console.WriteLine(dequeue2.PeekFromFront());
+            Console.WriteLine(dequeue2.PeekFromEnd() + "\n -----");
+            dequeue2.AddToFront("best");
+            dequeue2.AddToEnd("west");
+            Console.WriteLine(dequeue2.PeekFromFront());
+            Console.WriteLine(dequeue2.PeekFromEnd() + "\n -----");
+
+            Console.Read();
+        }
+    }
+}
---
+++ Week4day1_Tuesday/GenericDequeue/GenericDequeueLibrary/GenericDequeueLibrary.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDequeueLibrary
+{
+    public class GenericDequeue<T>
+    {
+        private List<T> list;
+
+        public GenericDequeue(List<T> list)
+        {
+            this.list = list;
+        }
+
+        public void Clear()
+        {
+            list.Clear();
+        }
+
+        public bool Contains(T objToSearch)
+        {
+            if (list.Contains(objToSearch))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public T RemoveFromFront()
+        {
+            T objToRemove;
+            if (list.Count != 0)
+            {
+                objToRemove = list[0];
+                list.Remove(objToRemove);
+                return objToRemove;
+            }
+            else
+            {
+                throw new ArgumentException("The list is empty!");
+            }
+        }
+
+
+        public T RemoveFromEnd()
+        {
+            T objToRemove;
+            if (list.Count != 0)
+            {
+                objToRemove = list[list.Count - 1];
+                list.Remove(objToRemove);
+                return objToRemove;
+            }
+            else
+            {
+                throw new ArgumentException("The list is empty!");
+            }
+
+        }
+
+        public void AddToFront(T objToAdd)
+        {
+            list.Insert(0, objToAdd);
+        }
+
+        public void AddToEnd(T objToAdd)
+        {
+            list.Insert(list.Count, objToAdd);
+        }
+
+        public T PeekFromFront()
+        {
+            T objToPeek;
+            if (list.Count != 0)
+            {
+                objToPeek = list[0];
+                return objToPeek;
+            }
+            else
+            {
+                throw new ArgumentException("The list is empty!");
+            }
+        }
+
+        public T PeekFromEnd()
+        {
+            T objToPeek;
+            if (list.Count != 0)
+            {
+                objToPeek = list[list.Count - 1];
+                return objToPeek;
+            } else
+            {
+                throw new ArgumentException("The list is empty!");
+            }
+        }
+    }
+}
