d7a096f10d6d43cd1a525975ab957f4931e1d75a
IvayloIvanov <ivailo.ivanov3@gmail.com>
2015-12-27 15:03:53 +0200

    week4day1

---
+++ Week4day1_Tuesday/GenericStackLibrary/GenericStackApplication/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GenericStackLibrary;
+
+
+namespace GenericStackApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list1 = new List<int>() {1,3,5,7,9,12,14,15};          
+
+            GenericStack<int> stack1 = new GenericStack<int>(list1);
+
+            Console.WriteLine(stack1.Pop());
+            Console.WriteLine(stack1.Peek());
+            stack1.Push(42);
+            Console.WriteLine(stack1.Peek());
+            Console.WriteLine(stack1.Contains(23));
+            Console.WriteLine("\n");
+
+            List<string> list2 = new List<string>() { "test", "beta", "delta", "out", "of ideas", " JK" };
+
+            GenericStack<string> stack2 = new GenericStack<string>(list2);
+
+            Console.WriteLine(stack2.Pop());
+            Console.WriteLine(stack2.Peek());
+            stack2.Push("Hola");
+            Console.WriteLine(stack2.Peek());
+            Console.WriteLine(stack2.Contains("beta"));
+            Console.Read();
+        }
+    }
+}
---
+++ Week4day1_Tuesday/GenericStackLibrary/GenericStackLibrary/GenericStack.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericStackLibrary
+{
+    public class GenericStack<T>
+    {
+        private List<T> list = new List<T>();
+
+
+        public GenericStack(List<T> list)
+        {
+            this.list = list;
+        }
+
+        public T Peek()
+        {
+            T TopObj;
+            if (list.Count != 0)
+            {
+                TopObj = list[list.Count - 1];
+                return TopObj;
+            }
+            else
+            {
+                TopObj = default(T);
+                return TopObj;
+            }
+        }
+
+        public T Pop()
+        {
+            T RemoveTopObj;
+            if (list.Count != 0)
+            {
+                RemoveTopObj = list[list.Count - 1];
+                list.Remove(RemoveTopObj);
+                return RemoveTopObj;
+            }
+            else
+            {
+                throw new ArgumentException("The list is empty.");
+            }
+        }
+
+        public void Push(T objToAdd)
+        {
+            list.Add(objToAdd);
+        }
+
+        public void Clear()
+        {
+            list.Clear();
+        }
+
+        public bool Contains(T objToSearch)
+        {
+            if (list.Contains(objToSearch))
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+    }
+}
