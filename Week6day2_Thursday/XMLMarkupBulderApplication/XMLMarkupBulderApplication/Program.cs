dc6f19523602cf5c10616df1f5c320ef0f3a7bcc
IvayloIvanov <ivailo.ivanov3@gmail.com>
2016-01-09 13:41:39 +0200

    week6day2

---
+++ Week6day2_Thursday/XMLMarkupBulderApplication/XMLMarkupBulderApplication/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XMLMarkupLibrary;
+
+namespace XMLMarkupBulderApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XMLMarkupBuilder builder = new XMLMarkupBuilder();
+
+            builder.openTag("Tag");
+            builder.addAttribute("ID", "25");
+            builder.openTag("Tag2");
+            builder.addText("Random text was added");
+            builder.closeTag();
+            builder.openTag("Tag3");
+            builder.addAttribute("Name", "Bob");
+            builder.finish();
+
+            Console.WriteLine(builder.getResult());
+            Console.Read();
+        }
+    }
+}
---
+++ Week6day2_Thursday/XMLMarkupBulderApplication/XMLMarkupLibrary/XMLMarkupBuilderLibrary.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLMarkupLibrary
+{
+    public class XMLMarkupBuilder
+    {
+        private Stack<string> tagHolder = new Stack<string>();
+        private StringBuilder xmlBuilder = new StringBuilder();
+
+        public XMLMarkupBuilder()
+        {
+            //tagHolder.Clear();
+            //xmlBuilder.Clear();
+        }
+
+
+        public XMLMarkupBuilder openTag(string tagName)
+        {
+            string openHelper = "<" + tagName + ">";
+            string closeHelper = "</" + tagName + ">";
+
+            xmlBuilder.Append(openHelper);
+            tagHolder.Push(closeHelper);
+
+            return this;
+        }
+
+
+
+        public XMLMarkupBuilder addAttribute(string attributeName,string attributeValue)
+        {
+            string attribute = string.Format(" " + attributeName + "={0}", attributeValue);
+
+            if (tagHolder.Count != 0)
+            {
+                if (xmlBuilder[xmlBuilder.Length - 1] == '>')
+                {
+                    xmlBuilder.Insert(xmlBuilder.Length - 1, attribute);
+                }
+                else
+                {
+                    throw new ArgumentException("Does not contain a closing tag '>'.");
+                }
+            } else
+            {
+                throw new ArgumentException("There are no open tags.");
+            }
+            return this;
+        }
+
+
+        public XMLMarkupBuilder addText(string text)
+        {
+
+            xmlBuilder.Append(text);
+            return this;
+        }
+
+
+        public XMLMarkupBuilder closeTag()
+        {
+            string helperTag = tagHolder.Pop();
+            xmlBuilder.Append(helperTag);
+            return this;
+        }
+
+
+        public XMLMarkupBuilder finish()
+        {
+            List<string> finishHelper = new List<string>();
+            while(tagHolder.Count != 0)
+            {
+                finishHelper.Add(tagHolder.Pop());
+            }
+
+            foreach (var item in finishHelper)
+            {
+                xmlBuilder.Append(item);
+            }
+
+            return this;
+        }
+
+        public String getResult()
+        {
+            return xmlBuilder.ToString();
+        }
+
+    }
+}
